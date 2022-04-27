a48634c62999077d119e79f79d5d5a8bba60a938
BPayne22 <pay18009@byui.edu>
2022-04-27 13:43:15 -0600

    new js code for grabbing year

--- js/dates.js
+++ js/dates.js
@@ -1,8 +1,12 @@
-const d = new Date();
-const year = d.getFullYear();
-
 //This will grab the current year from the funtion above
-document.getElementById("currentyear").textContent = year;
+const year = document.querySelector('#currentyear');
+
+try {
+    const options = {year: 'numeric'};
+    year.textContent = new Date().toLocaleDateString('en-UK', options);
+} catch (e) {
+    alert('Error with code or your browser does not support Locale.');
+}
 
 //This will prompt the date the file was changed. 
 document.getElementById("lastmodified").textContent = alert(document.lastModified);
