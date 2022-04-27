b616b16332933bd9e0f9fcd732b4eae382767868
BPayne22 <pay18009@byui.edu>
2022-04-27 13:48:30 -0600

    removed catch error

--- js/dates.js
+++ js/dates.js
@@ -4,9 +4,7 @@ const year = document.querySelector('#currentyear');
 try {
     const options = {year: 'numeric'};
     year.textContent = new Date().toLocaleDateString('en-UK', options);
-} catch (e) {
-    alert('Error with code or your browser does not support Locale.');
-}
+} 
 
 //This will prompt the date the file was changed. 
 document.getElementById("lastmodified").textContent = alert(document.lastModified);
