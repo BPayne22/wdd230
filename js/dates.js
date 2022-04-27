f5e02d14a53f27466c34a8f7c1a1356ab3596f7b
BPayne22 <pay18009@byui.edu>
2022-04-27 14:05:27 -0600

    testing modified variable

--- js/dates.js
+++ js/dates.js
@@ -1,3 +1,4 @@
+const modified = document.lastModified;
 //This will grab the current year from the funtion above
 const year = document.querySelector('#currentyear');
 
@@ -5,9 +6,9 @@ try {
     const options = {year: 'numeric'};
     year.textContent = new Date().toLocaleDateString('en-UK', options);
 } catch (e) {
-    alert(document.lastModified);
+    alert(modified);
 }
 
 //This will prompt the date the file was changed. 
-const modified = document.lastModified;
+
 document.getElementById("lastmodified").textContent = modified;
