c4555f1880ce8e85951e14d56eb1c895acdf4b4f
BPayne22 <pay18009@byui.edu>
2022-04-27 13:49:10 -0600

    revert change to catch

--- js/dates.js
+++ js/dates.js
@@ -4,7 +4,9 @@ const year = document.querySelector('#currentyear');
 try {
     const options = {year: 'numeric'};
     year.textContent = new Date().toLocaleDateString('en-UK', options);
-} 
+} catch (e) {
+    alert('Error with code or your browser does not support Locale.');
+}
 
 //This will prompt the date the file was changed. 
 document.getElementById("lastmodified").textContent = alert(document.lastModified);
