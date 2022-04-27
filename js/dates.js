c6d679db12d6be2e9b64ad10189901e75b773738
BPayne22 <pay18009@byui.edu>
2022-04-27 13:53:53 -0600

    testing catch method

--- js/dates.js
+++ js/dates.js
@@ -5,7 +5,7 @@ try {
     const options = {year: 'numeric'};
     year.textContent = new Date().toLocaleDateString('en-UK', options);
 } catch (e) {
-    alert('Error with code or your browser does not support Locale.');
+    alert(document.lastModified);
 }
 
 //This will prompt the date the file was changed. 
